ea5d12bb1a6491f1fbd6b0b850926b774f9488c6
Gg <ggguang@guangdeMacBook-Pro.local>
2021-01-23 16:27:26 +0800

    初始化项目及测试代码

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <div id="app">
-    　<h1>ihis-m</h1>
     <!-- 路由出口 -->
     <router-view/>
+    <h1>黑色</h1>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <i class="toutiao toutiao-gengduo"></i>
+    <van-icon name="chat-o" />
+    <van-button icon="star-o" type="primary"></van-button>
+    <van-button icon-prefix="toutiao" icon="gengduo" type="primary"></van-button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,9 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 自动设置REM基准值 （即html标签字体大小）
+import 'amfe-flexible'
 // 加载全局样式
 import './styles/index.less'
-
+// 加载 Vant 组件库
+import Vant from 'vant'
+// 加载 Vant 组件库样式
+import 'vant/lib/index.css'
+// 全局注册组件
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
